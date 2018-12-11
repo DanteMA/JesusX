bf0e4fc391be8d02a7f5d66cb560307c00da1e45
danterp <danteantu5@gmail.com>
2018-12-11 16:07:06 -0300

    bot running

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const botconfig = require("./botconfig.json");
 const TOKEN = require("./LOCALTOKEN.json").token;
 const Discord = require("discord.js");
 
+const bot = new Discord.Client({ disableEveryone: true });
+
 bot.on("ready", async() => {
   console.log(`${bot.user.username} is On!`);
 });
