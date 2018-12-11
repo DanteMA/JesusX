9362a46dc6321c1b98b95e0faf178120d8b00898
danterp <danteantu5@gmail.com>
2018-12-11 16:08:57 -0300

    update

--- src/index.js
+++ src/index.js
@@ -8,4 +8,18 @@ bot.on("ready", async() => {
   console.log(`${bot.user.username} is On!`);
 });
 
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if(cmd === `${prefix}hello`){
+    return message.channel.send("From the ashes, I have returned");
+  }
+});
+
 bot.login(TOKEN);
