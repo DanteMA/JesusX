bc526d7bd50e8c907bcbbc23288227f57e1b0d07
Dante Dekstop <danteantu5@gmail.com>
2018-12-13 14:42:39 -0300

    add update command

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const botconfig = require("./botconfig.json");
 const TOKEN = require("./LOCALTOKEN.json").token;
 const Discord = require("discord.js");
+var shell = require('shelljs');
 
 const bot = new Discord.Client({ disableEveryone: true });
 
@@ -17,6 +18,17 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
+  if(cmd === `${prefix}update`) {
+    // currently just 1 developer todo: add an array of developers unique id
+    if(message.author.discriminator == '0636') {
+      message.channel.send("Actualizando...")
+      shell.exec("npm install;git pull;pm2 restart JesusX");
+      return message.channel.send("Actualizacion completa!")
+    }
+    else {
+      return message.channel.send("Sorry, you cannot order me.")
+    }
+  }
   if(cmd === `${prefix}hello`){
     return message.channel.send("From the ashes, I have returned");
   }
---
+++ todo.md
@@ -0,0 +1,4 @@
+* Setup the bot : Done
+* Add a todo list managed by the bot itself :
+* Add "delete messages" feature :
+* Add an rebootAndUpdate function where the bot restarts while pulling the changes from the git repository :
